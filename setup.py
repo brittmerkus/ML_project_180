927f3dd94d1282f1ca4c49333086b63f2d36a749
gracibil <76951212+gracibil@users.noreply.github.com>
2025-03-10 12:30:25 +0100

    Updated setup,py to install to working directory,added Requirements.txt file to download dependencies, created folder for feature extraction functions, added empty files for our chosen feature extraction methods

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import kagglehub
+import os
 
+current_dir = os.getcwd()
+os.environ['KAGGLEHUB_CACHE'] = current_dir
 # Download latest version
 path = kagglehub.dataset_download("alessandrasala79/ai-vs-human-generated-dataset")
 
